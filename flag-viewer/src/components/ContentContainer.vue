d52028c8c460f1ee362f5aed113b5b52af966bed
Adam Leis <aleis@iti4dmv.com>
2022-04-23 04:07:04 -0400

    add ContentContainer presentation component

---
+++ flag-viewer/src/components/ContentContainer.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="contentContainer px-3">
+        <slot />
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+@import 'primeflex/src/utils';
+
+.contentContainer {
+    @include styleclass('px-1');
+    margin: 0 auto;
+    max-width: 1200px;
+}
+</style>
